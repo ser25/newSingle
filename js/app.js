bcedbf8407bda27a2ebbb05264d916b5be2e454e
ser25 <shatsovniy25@gmail.com>
2022-07-17 23:00:06 +0300

    Player 1

--- js/app.js
+++ js/app.js
@@ -1,14 +1,18 @@
-const players = document.querySelectorAll('.bodySingle');
+const player1 = document.querySelector('.newSingle__body');
+const player2 = document.querySelector('.LastTracke__body');
 const playBtn = document.querySelectorAll('.plays');
+const audio1 = document.querySelector('.newSingle__song');
 const audio = document.querySelector('.newSingle__song');
+const audio2 = document.querySelector('.LastTracke__song');
 const progressContainer = document.querySelector('.newSingle__bar ');
-const progress = document.querySelector('.bar__progress');
+const progressAll = document.querySelectorAll('.bar__progress');
 const title = document.querySelector('.newSingle__songName');
 const cover = document.querySelector('.player__img');
 const imgSrcs = document.querySelectorAll('.Play');
-const time = document.querySelector('.time1');
-const timeFull = document.querySelector('.time2');
+const times = document.querySelectorAll('.time1');
+const timesFull = document.querySelectorAll('.time2');
 const nextBtn = document.querySelector('.playPause__item_2');
+const prevBtn = document.querySelector('.playPause__item_1');
 //Назви пісень
 const songsArray = ['Let me down slowly',
                      'Summertime Sadness',
@@ -17,6 +21,8 @@ const songsArray = ['Let me down slowly',
                       'Don’t Call Me Angel',
                       'la di die'
                     ];
+                    const songsArrayNewSingl = ['Let me down slowly',
+                   ];                  
 //Пісня по умолчанію
 let songIndex = 0;
 //init 
@@ -25,9 +31,16 @@ function loadSong(song) {
     title.innerHTML = song;
     audio.src = `music/${song}.mp3`;
 }  
+loadSong(songsArrayNewSingl[songIndex]);
 loadSong(songsArray[songIndex]);
 //Play
 function playSong(item) {
+    console.log(`item: ${item}`);
+    /*let isPlayer1 = item.closest('.newSingle__body');
+    let isPlayer2 = item.closest('.LastTracke__body');
+    console.log(`1: ${isPlayer1}`);
+    console.log(`2: ${isPlayer2}`);*/
+
            // item.classList.add('._play');
             audio.play();
             /*imgSrcs.forEach(item => {
@@ -36,8 +49,13 @@ function playSong(item) {
             //imgSrcs.src = `/images/newSingl/pause-solid.svg`;
             if (!(item === undefined)) {
                 let imageScr = item.querySelector('.Play');
-                imageScr.src = `/images/newSingl/pause-solid.svg`;
-            }
+                imageScr.src = `/images/newSingl/pause-solid.svg`; 
+            };
+    /*playBtn.forEach(play => {
+
+    })      
+    player.classList.add('_play');
+    audio.play();    */    
 
 }
 //Pause
@@ -48,9 +66,30 @@ function pauseSong(item) {
         let imageScr = item.querySelector('.Play');
         imageScr.src = `/images/newSingl/play-solid.svg`;
     }
-
-    
 }
+playBtn.forEach(function(item){
+    item.addEventListener('click', () => {
+        /*let isPlayer1 = item.closest('.newSingle__body');
+        let isPlayer2 = item.closest('.LastTracke__body');
+        if(isPlayer1) {
+            const isPlaying = isPlayer1.classList.add('_play1');
+            console.log(`1: ${isPlaying}`);
+            
+        } else if(isPlayer2) {
+            const isPlaying = isPlayer2.classList.add('_play2');
+            console.log(`2: ${isPlaying}`);
+        }*/
+        let necessaryBtn = item.classList.toggle('_is'); // цвідци прац
+        //let ne = item;
+        if (necessaryBtn) {
+            //pauseSong();
+            playSong(item);
+        } else {
+           // playSong();
+            pauseSong(item);
+        }
+    });
+});
 
 
 function nextSong() {
@@ -59,11 +98,14 @@ function nextSong() {
         songIndex = 0;
     }
     loadSong(songsArray[songIndex]);
-    playSong();
+    let weq = player2.querySelector('.Play')
+    console.log(weq);
+    audio.play();
+    weq.src = `/images/newSingl/pause-solid.svg`;
+    let qws = player2.querySelector('.plays')
+    let qwws = qws.classList.add('_is')
+
 }
-/*nextBtn.forEach(item => {
-    item.addEventListener('click', nextSong);
-})*/
 nextBtn.addEventListener('click', nextSong);
 
 function prevSong() {
@@ -72,17 +114,23 @@ function prevSong() {
         songIndex = songsArray.length - 1;
     }
     loadSong(songsArray[songIndex]);
-    playSong();
+    let weq = player2.querySelector('.Play')
+    console.log(weq);
+    audio.play();
+    weq.src = `/images/newSingl/pause-solid.svg`;
+    let qws = player2.querySelector('.plays')
+    let qwws = qws.classList.add('_is')
 }
-//prevBtn.addEventListener('click', prevSong);
+prevBtn.addEventListener('click', prevSong);
 
 function updateProgress(e) {
     const {duration, currentTime} = e.srcElement;
     //console.log(duration);
     //console.log(currentTime);
     const progressPercent = (currentTime / duration) * 100;
-    progress.style.width = `${progressPercent}%`;
-
+    progressAll.forEach(progress =>{
+        progress.style.width = `${progressPercent}%`;
+    });
     //Minutes
     let minutes = Math.floor(audio.currentTime / 60);
     let minutesFull = Math.floor(audio.duration / 60);
@@ -101,13 +149,14 @@ function updateProgress(e) {
     if (secondsFull < 10 ) {
         secondsFull = '0' + String(secondsFull);    
     }
-
-    time.innerHTML =`${minutes}:${seconds}`;
-    if(!isNaN(minutesFull) && !isNaN(secondsFull)) {
-        timeFull.innerHTML =`${minutesFull}:${secondsFull}`;
-    }
-    
-
+    times.forEach(time =>{
+        time.innerHTML =`${minutes}:${seconds}`;
+    })
+    timesFull.forEach(timeFull => {
+        if(!isNaN(minutesFull) && !isNaN(secondsFull)) {
+            timeFull.innerHTML =`${minutesFull}:${secondsFull}`;
+        }
+    })
 
 }
 audio.addEventListener('timeupdate', updateProgress);
@@ -130,7 +179,8 @@ function infinity() {
 
 //AutoPlay
 
-audio.addEventListener('ended', nextSong);
+audio1.addEventListener('ended', infinity);
+audio2.addEventListener('ended', nextSong);
 
 /*playBtn.addEventListener('click', () => {
     const isPlaying = player.classList.contains('_play');
@@ -158,18 +208,5 @@ audio.addEventListener('ended', nextSong);
         
     });
 });*/
-playBtn.forEach(function(item){
-    item.addEventListener('click', () => {
-        let necessaryBtn = item.classList.toggle('_is');
-        //let ne = item;
-        if (necessaryBtn) {
-            //pauseSong();
-            playSong(item);
-           
-        } else {
-           // playSong();
-            pauseSong(item);
-        }
 
-    });
-});    
+    
