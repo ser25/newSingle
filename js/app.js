c2c1da3d5a5e024a8403fdb87edf76746b8c875c
ser25 <shatsovniy25@gmail.com>
2022-07-21 14:36:02 +0300

    update work all except list songs

--- js/app.js
+++ js/app.js
@@ -4,15 +4,15 @@ const playBtn = document.querySelectorAll('.playPause__item_play');
 const audio1 = document.querySelector('.newSingle__song');
 //const audio = document.querySelector('.newSingle__song');
 const audio2 = document.querySelector('.LastTracke__song');
-const progressContainerNewSingle = document.querySelector('.newSingle__bar ');
+const progressContainer = document.querySelectorAll('.bar');
 const progressContainerLastTracke = document.querySelector('.LastTracke__bar ');
 const progressNewSingle = document.querySelector('.bar__progress_newSingle');
 const progressLastTracke = document.querySelector('.bar__progress_LastTracke');
 const title = document.querySelector('.newSingle__songName');
 let cover = document.querySelector('[data-image]');
 const imgSrcs = document.querySelectorAll('.Play');
-const times = document.querySelectorAll('.time1');
-const timesFull = document.querySelectorAll('.time2');
+const time = document.querySelectorAll('.time1');
+const timeFull = document.querySelectorAll('.time2');
 const nextBtn = document.querySelector('.playPause__item_2');
 const prevBtn = document.querySelector('.playPause__item_1');
 //Назви пісень
@@ -65,10 +65,14 @@ function playSong() {
         player1.classList.add('_play');
         audio1.play();
         imgPlay.src = `images/newSingl/pause-solid.svg`;
+        audio1.addEventListener('timeupdate', updateProgress);
+        audio1.addEventListener('ended', infinity);  
     } else if(necessaryPlayer2){
         player2.classList.add('_play');
         audio2.play();
         imgPlay.src = `images/newSingl/pause-solid.svg`;
+        audio2.addEventListener('timeupdate', updateProgress);
+        audio2.addEventListener('ended', nextSong);
     }
 
 }
@@ -96,7 +100,6 @@ function pauseSong() {
 //Start
 playBtn.forEach(function(item){
     item.addEventListener('click', function()  {
-      
         let isPlayer = item.closest('.newSingle__body') || item.closest('.LastTracke__body');
         let isPlaying = isPlayer.classList.contains('_play');
         offPlayer2OnPlayer1();
@@ -144,13 +147,17 @@ function prevSong() {
 }
 prevBtn.addEventListener('click', prevSong);
 
-function updateProgress(e, progress, time) {
+function updateProgress(e) {
     const {duration, currentTime} = e.srcElement;
     const progressPercent = (currentTime / duration) * 100;
+    let necessaryPlayer = this.closest('[data-body]');
+    console.log(necessaryPlayer);
+    let progress = necessaryPlayer.querySelector('.bar__progress');
+    //console.log(progress)
     progress.style.width = `${progressPercent}%`;
     //Minutes
-    let minutes = Math.floor(audio.currentTime / 60);
-    let minutesFull = Math.floor(audio.duration / 60);
+    let minutes = Math.floor(this.currentTime / 60);
+    let minutesFull = Math.floor(this.duration / 60);
     if (minutes < 10 ) {
         minutes = '0' + String(minutes);    
     }
@@ -158,44 +165,50 @@ function updateProgress(e, progress, time) {
         minutesFull = '0' + String(minutesFull);    
     }
     //Seconds
-    let seconds = Math.floor(audio.currentTime % 60);
-    let secondsFull = Math.floor(audio.duration % 60);
+    let seconds = Math.floor(this.currentTime % 60);
+    let secondsFull = Math.floor(this.duration % 60);
     if (seconds < 10 ) {
         seconds = '0' + String(seconds);
     }
     if (secondsFull < 10 ) {
         secondsFull = '0' + String(secondsFull);    
     }
+    let time = necessaryPlayer.querySelector('.time1')
+    let timeFull = necessaryPlayer.querySelector('.time2')
     time.innerHTML =`${minutes}:${seconds}`;
     if(!isNaN(minutesFull) && !isNaN(secondsFull)) {
             timeFull.innerHTML =`${minutesFull}:${secondsFull}`;
     }
 
 }
-updateProgress(progressContainerNewSingle, progressNewSingle);
-audio1.addEventListener('timeupdate', updateProgress);
-audio2.addEventListener('timeupdate', updateProgress);
+//audio1.addEventListener('timeupdate', updateProgress);
+//audio2.addEventListener('timeupdate', updateProgress);
 
 function setProgress(e) {
+    console.log(this);
+    let isPlayer = this.closest('[data-body]')
+    let audio = isPlayer.querySelector('[data-audio]')
     const width = this.clientWidth;
     const clickX = e.offsetX;
-    const duration = audio1.duration;
+    const duration = audio.duration;
 
-    audio1.currentTime = (clickX / width) * duration;
-
+    audio.currentTime = (clickX / width) * duration;
+    offPlayer2OnPlayer1();
+    let addPlayer = isPlayer.classList.add('_is');
+    let addImg = isPlayer.querySelector('.Play')
+    addImg = addImg.classList.add('_img');
+    offPlayer1OnPlayer2();
+    playSong();
 }
-progressContainer.addEventListener('click', setProgress);
+progressContainer.forEach(function(item){
+    item.addEventListener('click', setProgress);
+})
+//progressContainer.
 
 function infinity() {
     songIndex = songsArray.length -1;
     loadSong(songsArray[songIndex]);
     playSong();
 }
-
-//AutoPlay
-
-audio1.addEventListener('ended', infinity);
-audio2.addEventListener('ended', nextSong);
-
 
     
